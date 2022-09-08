4403352a4ada2bf74001f7ff2225b9fb1a5a47e8
Louie Cagas <louiecagas1210@gmail.com>
2022-09-08 22:43:08 +0800

    9/8/2022

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -14,8 +15,14 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (User::exists()) {
+            if (!$request->expectsJson()) {
+                return route('login');
+            }
+        } else {
+            if (!$request->expectsJson()) {
+                return route('register');
+            }
         }
     }
 }
